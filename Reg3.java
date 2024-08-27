848d2407d36a7d996492958a0792d32c9476320f
Shashi preetham Nathi <nathishashipreetham@gmail.com>
2024-08-27 23:35:35 +0530

    Update Reg3.java

--- Reg3.java
+++ Reg3.java
@@ -1,3 +1,19 @@
 public class Reg3{
-// Extract all the digits from a string.
+
+import java.util.Scanner;
+
+public class ExtractDigits {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("enter the values");
+        String input=sc.nextLine();
+        
+
+        // Use a regular expression to extract all digits
+        String digits = input.replaceAll("[^0-9]", "");
+
+        System.out.println("Extracted digits: " + digits);
+    }
+}
+
 }
