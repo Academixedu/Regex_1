6af2876878345a360c7f879d9c166d212711f022
kavitha-guntru <kavithaguntru60@gmail.com>
2024-08-27 18:48:44 +0530

    kavitha

--- Reg2.java
+++ Reg2.java
@@ -1,3 +1,23 @@
-public class Reg2{
-// Prepare a Regex to Find if a string contains the word "Java".
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Reg2 {
+    // Prepare a Regex to Find if a string contains the word "Java".
+    public static void literalExample(String pattern) {
+        System.out.println(" Match the data:");
+        String text = "The Java based concepts.";
+        
+        Pattern k1 = Pattern.compile(pattern);
+        Matcher s = k1.matcher(text);
+        
+        while (s.find()) {
+            System.out.println("Found '" + s.group() + "' at index " + s.start());
+        }
+        System.out.println();
+    }
+    
+    public static void main(String[] args) {
+        Reg2 k = new Reg2();
+        k.literalExample("Java"); 
+    }
 }
