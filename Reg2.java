ed2966b28a1a720d1c1e1fcbde5644bd5b92acab
gsbhavani <suryagogadi02@gmail.com>
2024-08-27 19:12:36 +0530

    completed

--- Reg2.java
+++ Reg2.java
@@ -1,3 +1,16 @@
+import java.util.*;
 public class Reg2{
 // Prepare a Regex to Find if a string contains the word "Java".
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        String regex = "Java";
+        if(str.matches(regex))
+            System.out.println("Yes");
+        else
+            System.out.println("No");
+        sc.close();
+    }
 }
+
