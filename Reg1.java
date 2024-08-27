d270e2594c463b05e1c9e6b75cffb2061a3b1a64
nirajpavankumar <nirajpavankumar1282@gmail.com>
2024-08-27 20:21:49 +0530

    regex done

--- Reg1.java
+++ Reg1.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
+
 public class Reg1{
 // Prepare a Regex for Email Validation
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Email: ");
+        String email = sc.nextLine();
+        String regexpattern = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9]+[.][a-zA-Z]+$";
+        if(email.matches(regexpattern))
+            System.out.println(" it is a Valid Email");
+        else
+            System.out.println(" it is a Invalid Email");
+        sc.close();
+    }
 }
--- Reg3.java
+++ Reg3.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Reg3{
 // Extract all the digits from a string.
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String: ");
+        String str = sc.nextLine();
+        String regex = "\\d+";
+        Pattern p = Pattern.compile(regex);
+        Matcher m = p.matcher(str);
+        while (m.find()) {
+            System.out.println(m.group());
+        }
+        sc.close();
+    }
 }
