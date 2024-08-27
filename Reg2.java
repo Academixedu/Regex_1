c9ce1ad5dcb22fe97d0dba19bd21f9fa3453e652
MekaManojKrishnalakshman <mekamanojkrishnalakshman@gmail.com>
2024-08-27 18:25:33 +0530

    regex

--- Reg2.java
+++ Reg2.java
@@ -1,3 +1,17 @@
+import java.util.regex.*;
+import java.util.Scanner;
 public class Reg2{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        String regex="\\bJava\\b";
+        Pattern p=Pattern.compile(regex);
+        Matcher m=p.matcher(str);
+        if(m.find()){
+            System.out.println("word Found");
+        }
+        else
+            System.out.println("word Not Found");
+    }
 // Prepare a Regex to Find if a string contains the word "Java".
 }
