d71d9ad657e6f68a2a267e45b458b8b574548560
saikumar34892 <saikumarmatta3821@gmail.com>
2024-08-27 20:19:47 +0530

    from sai

--- Reg1.java
+++ Reg1.java
@@ -1,3 +1,27 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Reg1{
 // Prepare a Regex for Email Validation
+public static void emailvalidation(String email){
+
+    String pattern = "^[\\w.%+-]+@[\\w.-]+\\.[A-Za-z]{2,6}$";
+
+
+    Pattern p = Pattern.compile(pattern);
+    Matcher m = p.matcher(email);
+
+    if(m.matches()){
+        System.out.println("Valid email");
+    }
+    else{
+        System.out.println("Invalid email");
+    }
+    System.err.println(" ");
+
+}
+public static void main(String[] args) {
+    Reg1 r = new Reg1();
+    r.emailvalidation("saikumarmatta3821@gmail.com");
+}
 }
