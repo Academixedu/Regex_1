a3ebb47b7263be5f8cc43ce03efb60e9effbad99
Pavani23678 <pavaniburadhakavi@gmail.com>
2024-08-27 18:27:04 +0530

    regex

--- Reg1.java
+++ Reg1.java
@@ -1,3 +1,23 @@
-public class Reg1{
+
 // Prepare a Regex for Email Validation
-}
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class Reg1{
+
+
+     static final String EMAIL_REGEX = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+
+    public static boolean isValidEmail(String email) {
+        Pattern pattern = Pattern.compile(EMAIL_REGEX);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    public static void main(String[] args) {
+
+        String email = "pavaniburadakavi@gmail.com";
+            System.out.println(email + ": " + isValidEmail(email));
+        }
+    }
+
--- Reg2.java
+++ Reg2.java
@@ -1,3 +1,23 @@
-public class Reg2{
+
 // Prepare a Regex to Find if a string contains the word "Java".
-}
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+public class Reg2 {
+
+    // Prepare a Regex to find if a string contains the word "Java"
+     static final String JAVA_REGEX = "\\bJava\\b";
+
+    public static boolean containsJava(String input) {
+        Pattern pattern = Pattern.compile(JAVA_REGEX);
+        Matcher matcher = pattern.matcher(input);
+        return matcher.find();
+    }
+
+    public static void main(String[] args) {
+        // Test cases
+        String input = "I love Java programing";
+            System.out.println("\"" + input + "\": " + containsJava(input));
+        }
+    }
+
