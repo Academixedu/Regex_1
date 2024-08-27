fb416394aec8e3821a69d1e129bfbf97c86fa444
Sarveshbramhane <139378858+Sarveshbramhane@users.noreply.github.com>
2024-08-27 20:11:02 +0530

    sarvesh

--- Reg1.java
+++ Reg1.java
@@ -1,3 +1,27 @@
-public class Reg1{
+
+import java.util.*;
+public class Reg1
+{
 // Prepare a Regex for Email Validation
+public static void main(String[] args) 
+{
+   Scanner in =new Scanner(System.in);
+   System.out.println("Enter Your Email-Id => ");
+   String Emailid=in.nextLine();
+
+   String regex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+   boolean result=Emailid.matches(regex);
+
+   if(result)
+   {
+    System.out.println("Given Email-Id is Valid");
+   }
+   else 
+   {
+    System.out.println("Given Email-Id is Not Valid");
+   }
+   in.close();
+       
+}
+
 }
