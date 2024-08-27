5ad297b1cc816776610d202e4775e74c41c0a43d
srinadh0405 <srinadh.nallabothula@email.com>
2024-08-27 20:09:25 +0530

    regex1

--- Reg1.java
+++ Reg1.java
@@ -1,3 +1,16 @@
+import java.util.Scanner;
 public class Reg1{
 // Prepare a Regex for Email Validation
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the email:");
+        String email = sc.nextLine();
+        String regex = "^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9]+[.][a-zA-Z]+$";
+        if(email.matches(regex))
+            System.out.println("Valid Email");
+        else
+            System.out.println("Invalid Email");
+        sc.close();
+    }
 }
--- Reg3.java
+++ Reg3.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Reg3{
 // Extract all the digits from a string.
+
+        public static void main(String[] args){
+        Scanner in= new Scanner(System.in);
+        System.out.println("Enter the Sentence:");
+        String str=in.nextLine();
+        String regex="\\d+";
+        Pattern p=Pattern.compile(regex);
+        Matcher M=p.matcher(str);
+        while(M.find()){
+            System.out.println(M.group());
+        }
+
+    }
 }
+
