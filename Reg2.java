b8d61d42e06b0660465eba2af30b9ba00aacd66f
Shreyash000007 <shreyasharg@gmail.com>
2024-08-27 19:06:11 +0530

    Shreyash

--- Reg2.java
+++ Reg2.java
@@ -1,3 +1,20 @@
-public class Reg2{
+package Regex_1;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Reg2
+{
 // Prepare a Regex to Find if a string contains the word "Java".
+public static void main(String[] args) {
+        String text = "I love programming in .";
+        Pattern pattern = Pattern.compile("\\bJava\\b");
+        Matcher matcher = pattern.matcher(text);
+
+        if (matcher.find()) {
+            System.out.println("Found Java in the text.");
+        } else {
+            System.out.println("Did not find Java in the text.");
+        }
+    }
+
 }
