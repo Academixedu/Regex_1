81d3557d1ea2595b2752d7f4cd2f874e69a41fbb
Verat-hub <venkey482thalka@gmail.com>
2024-08-27 19:06:51 +0530

    Venkat

--- Reg3.java
+++ Reg3.java
@@ -1,3 +1,19 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Reg3{
 // Extract all the digits from a string.
+public static void extractDigits(String input) {
+    String digitPattern = "\\d+";
+      Pattern pattern = Pattern.compile(digitPattern);
+       Matcher matcher = pattern.matcher(input);
+       while (matcher.find()) {
+        System.out.println("Found digits: " + matcher.group() + " at index " + matcher.start());
+    }
+}
+    public static void main(String[] args) {
+        String inputText = "1two3four5six7eight9ten";
+
+extractDigits(inputText);
+}
 }
