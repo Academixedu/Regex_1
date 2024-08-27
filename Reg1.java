981998bf648e8e900ad9ee59e642564c7c98819e
Ro-045 <pratapnaidu016@gmail.com>
2024-08-28 00:36:51 +0530

    pratap

--- Reg1.java
+++ Reg1.java
@@ -1,3 +1,31 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.*;
 public class Reg1{
 // Prepare a Regex for Email Validation
+//john.m.doe@businessname.com
+//jd123@businessname.com
+//john_doe@businessname.com
+// firstname.lastname@businessname.com
+//john_12.doe_12@acad.net
+
+public static void emailValidation(String email){
+    String pattern="(^//d([a-z0-9_]{1,15}\\.?){0,2}[a-z0-9_]{1,15})@[a-z]{1,12}\\.[a-z]{2,8}";
+    Pattern p=Pattern.compile(pattern);
+    Matcher m=p.matcher(email);
+    if (m.matches()) {
+        System.out.println("Valid email: " + email);
+    } else {
+        System.out.println("Invalid email: " + email);
+    }
+    System.out.println();
+}
+public static void main(String[] args) {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter email");
+    String email1=sc.nextLine();
+    emailValidation(email1);
+    sc.close();
+}
+
 }
