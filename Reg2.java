08dcfe87bb7806fc035bf474a9733df37472a94b
syamkarthik0 <dharmavarapukarthik@gmail.com>
2024-08-27 18:29:27 +0530

    From Karthik

--- Reg2.java
+++ Reg2.java
@@ -1,3 +1,18 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Reg2{
 // Prepare a Regex to Find if a string contains the word "Java".
+ public static void main(String args[]) {
+        System.out.println("Literal Matching:");
+        String pattern = "Java";
+        String text = "//+5Javay*5/";
+        
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(text);
+        
+        while (m.find()) {
+            System.out.println("Found '" + m.group() + "' at index " + m.start());
+        }
+    }
 }
