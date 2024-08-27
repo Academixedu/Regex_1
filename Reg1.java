fd9488f626644898954f4351295c50cd32b5a274
syamkuamrnallamilli0 <syamkumarnallamilli0@gmail.com>
2024-08-27 19:21:28 +0530

    done

--- Reg1.java
+++ Reg1.java
@@ -1,3 +1,28 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Reg1{
 // Prepare a Regex for Email Validation
+    public static void email(String email) {
+        
+        String pattern = "^[\\w]+@[\\w.-]+\\.[A-Za-z]{2,6}$";
+        
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(email);
+        
+        if (m.matches()) {
+            System.out.println("Valid email: " + email);
+        } else {
+            System.out.println("Invalid email: " + email);
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        // email("syam@123gmail.com");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter ur email");
+        String name=sc.nextLine();
+        email(name);
+    }
 }
--- Reg3.java
+++ Reg3.java
@@ -1,3 +1,23 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Reg3{
 // Extract all the digits from a string.
+    public static void main(String args[]){
+         String input = "java123script456prog7.8%9";
+
+        
+        String pattern = "\\d+";
+
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(input);
+
+        System.out.println("Extracted digits:");
+
+        while (m.find()) {
+            System.out.println(m.group());
+        }
+    }
 }
+    
+
