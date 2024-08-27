5b691243aa4f9764cc5ef73e2a766e408ddbdc97
siva-245711 <sivanagendra809@gmail.com>
2024-08-27 19:54:30 +0530

    regexx

--- Reg2.java
+++ Reg2.java
@@ -1,3 +1,16 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Reg2{
 // Prepare a Regex to Find if a string contains the word "Java".
-}
+ public static void main(String[] args) {
+        String string_java_partten = "^[a-zA-Z]\\bJava\\b[a-zA-Z]$";
+        Pattern pattern = Pattern.compile(string_java_partten);
+        Matcher matcher = pattern.matcher("I am learning Java at AcademiXedu.");
+
+        if (matcher.matches()) {
+            System.out.println("java matched");
+        } else {
+            System.out.println("java not matched");
+        }
+    }}
--- Reg3.java
+++ Reg3.java
@@ -1,3 +1,18 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Reg3{
 // Extract all the digits from a string.
+ public static void main(String[] args) {
+        String input = "My order number is 12345 and my zip code is 67890.";
+        String pattern = "\\d+";  
+
+        Pattern compiledPattern = Pattern.compile(pattern);
+        Matcher matcher = compiledPattern.matcher(input);
+
+        System.out.println("Digits found in the string:");
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
 }
