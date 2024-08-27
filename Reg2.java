dc207e17b958e1e8630779c5bc02f7dcab719165
jangamswarupa-595 <swarupajangam9@gmail.com>
2024-08-27 19:28:16 +0530

    swarupa

--- Reg2.java
+++ Reg2.java
@@ -1,3 +1,12 @@
 public class Reg2{
 // Prepare a Regex to Find if a string contains the word "Java".
+public static void main(String[] args){
+    String s1="Java Full Stack Development";
+    if(s1.contains("Java")){
+        System.out.println("yes: " +s1);
+    }
+    else{
+        System.out.println("not contain:  " +s1); 
+    }
+}
 }
--- Reg3.java
+++ Reg3.java
@@ -1,3 +1,13 @@
 public class Reg3{
 // Extract all the digits from a string.
+public static void main(String[] args) {
+    String s1="Swarupa12345@saru875";
+    String digit="";
+    for(char c:s1.toCharArray()){
+        if(Character.isDigit(c)){
+            digit=digit+c;
+        }
+    }
+    System.out.println(digit);
+}
 }
