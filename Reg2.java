87237c6d0bb9b3de38705c0ab7419a174050d88c
kurojulucky <luckykuroju28@gmail.com>
2024-09-11 19:47:47 +0530

    done

--- Reg2.java
+++ Reg2.java
@@ -1,3 +1,9 @@
 public class Reg2{
+    public static void main(String[] args) {
+    String text = "I love Java programming!";
+    String regex = "\\bJava\\b";
+    System.out.println(text.matches(".*" + regex + ".*") ? "Contains 'Java'" : "Does not contain 'Java'");
+
+    }
 // Prepare a Regex to Find if a string contains the word "Java".
 }
--- Reg3.java
+++ Reg3.java
@@ -1,3 +1,15 @@
+import java.util.regex.*;
 public class Reg3{
-// Extract all the digits from a string.
+    public static void main(String[] args) {
+        String text = "Hello123World456!";
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(text);
+        
+        while (matcher.find()) {
+            System.out.println(matcher.group());
+        }
+    }
 }
+
+// Extract all the digits from a string.
+
