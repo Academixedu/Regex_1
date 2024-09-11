7b9c8fef0548b476749639dd3fd62c04a1bf1466
revathivinnakota90@gmail.com <revathivinnakota90@gmail.com>
2024-09-11 12:39:21 +0530

    Revathi

--- Reg3.java
+++ Reg3.java
@@ -1,3 +1,22 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Reg3{
 // Extract all the digits from a string.
+public static void main(String[] args) {
+    digits();
 }
+public static void digits(){
+    String pattern = "\\d+";
+    String text="ip address is 198.162.280.9";
+    Pattern p = Pattern.compile(pattern);
+    Matcher m = p.matcher(text);
+    System.out.println("Digits");
+    while (m.find()) {  
+        System.out.println(m.group());  
+    }
+    System.out.println();
+
+}
+}
+
