2639e031511318ed1cd929a91cb5bb0f782f798b
Vishal <87086820+Vishalprepro@users.noreply.github.com>
2024-09-11 16:47:19 +0530

    Update Reg2.java

--- Reg2.java
+++ Reg2.java
@@ -1,3 +1,17 @@
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Reg2{
 // Prepare a Regex to Find if a string contains the word "Java".
+ public static void main(String[] args) {
+        String sc = "Java";
+        Scanner sc1 = new Scanner(System.in);
+        String text = sc1.nextLine();
+
+        Pattern p = Pattern.compile(sc);
+        Matcher m = p.matcher(text);
+        System.out.println(m.find());
+
+    }
 }
