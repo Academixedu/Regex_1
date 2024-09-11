2718c947c26b02461e98d948f2edb1690f80376a
kumar jyothiranjan <108728582+jyothiranjan25@users.noreply.github.com>
2024-09-11 17:47:50 +0530

    Update Reg2.java

--- Reg2.java
+++ Reg2.java
@@ -1,3 +1,16 @@
+import java.util.regex.Pattern;
+
 public class Reg2{
-// Prepare a Regex to Find if a string contains the word "Java".
+    // Prepare a Regex to Find if a string contains the word "Java".
+    private static final String STRING_REGEX = ".*\\bJava\\b.*";
+    private static final Pattern STRING_PATTERN = Pattern.compile(STRING_REGEX);
+
+    public static void main(String[] args) {
+        String string = "Java is a programming language";
+        if(STRING_PATTERN.matcher(string).matches()){
+            System.out.println("String contains the word Java");
+        }else{
+            System.out.println("String does not contain the word Java");
+        }
+    }
 }
