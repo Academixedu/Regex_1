557ef0a8938b4a1c531282e519c6a278fd835b03
Mohammad Shahid Afridi <142991991+Shahid-763@users.noreply.github.com>
2024-09-11 19:06:19 +0530

    shahid

--- Reg2.java
+++ Reg2.java
@@ -1,3 +1,21 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Reg2{
 // Prepare a Regex to Find if a string contains the word "Java".
+public static void word(){
+    Scanner x=new Scanner(System.in);
+    String s="java";
+    String text=x.nextLine();
+    Pattern p=Pattern.compile(s);
+    Matcher m=p.matcher(text);
+    while (m.find()) {
+        System.out.println(m.group()+"  "+m.start());
+    }
 }
+public static void main(String[] args) {
+    word();
+}
+}
+
