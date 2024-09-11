3f6fbbe3db700266b4bfa7700f0944f086624f3c
Aparna526 <aparnakondapaturu@gmail.com>
2024-09-11 11:36:48 +0530

    one

--- Reg1.java
+++ Reg1.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Reg1{
 // Prepare a Regex for Email Validation
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String pattern = sc.nextLine();
+    String email = sc.nextLine();
+
+    Pattern p = Pattern.compile(pattern);
+    Matcher m = p.matcher(email);
+
+    if(m.matches()){
+        System.out.println(email+" is Valid");
+    }
+    else{
+        System.out.println(email+" is Not Valid");
+    }
+}
 }
--- Reg3.java
+++ Reg3.java
@@ -1,3 +1,19 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Reg3{
 // Extract all the digits from a string.
+public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    String pattern = sc.nextLine();
+    String text = sc.nextLine();
+
+    Pattern p = Pattern.compile(pattern);
+    Matcher m = p.matcher(text);
+
+    while(m.find()){
+        System.out.println(m.group());
+    }
+}
 }
